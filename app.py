4819b336c12969625aefd95ec22ac8daf70f9493
Samael Sánchez <antonio.sanchez35@hotmail.com>
2025-07-20 22:37:57 -0600

    Init

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+db = SQLAlchemy()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
