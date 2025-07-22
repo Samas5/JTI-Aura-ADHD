dd7919b7bd3b2eccdd34d3a3381109e78c46c65b
Samael Sánchez <antonio.sanchez35@hotmail.com>
2025-07-21 19:44:28 -0600

    Corrección bug en import

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,11 +9,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
